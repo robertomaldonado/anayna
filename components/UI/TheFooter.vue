f384c1ee1aa06b4a91b68b8526632c656ec9418a
Rob <rbt.maldonado@gmail.com>
2020-12-14 21:25:58 -0500

    Added image overlay on click

--- components/UI/TheFooter.vue
+++ components/UI/TheFooter.vue
@@ -5,24 +5,57 @@
   >
     <span>&copy; {{ new Date().getFullYear() }}</span>
     <v-spacer/>
-    <v-icon class="mr-4">
-      mdi-facebook
-    </v-icon>
-    <v-icon class="mr-4">
-      mdi-instagram
-    </v-icon>
-    <v-icon>
-      mdi-whatsapp
-    </v-icon>
+      <div class="mr-4">
+        <v-btn
+          @click="toFacebook()"
+          color="primary"
+          fab
+          small
+          dark
+        >
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+      </div>
+      <div class="mr-4">
+        <v-btn
+          color="red"
+          fab
+          small
+          dark
+        >
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+      </div>
+      <div class="mr-4">
+        <v-btn
+          color="green"
+          fab
+          small
+          dark
+        >
+          <v-icon>mdi-whatsapp</v-icon>
+        </v-btn>
+      </div>
     <v-spacer/>
-    <v-icon>
-      mdi-contacts
-    </v-icon>
+      <div class="mr-4">
+        <v-btn
+          color="gray"
+          small
+          dark
+        >
+          <v-icon>mdi-contacts</v-icon>
+        </v-btn>
+      </div>
   </v-footer>
 </template>
 
 <script>
 export default {
+  methods : {
+    toFacebook () {
+      window.open("https://m.facebook.com/anayna.ec");
+    }
+  }
 }
 </script>
 
