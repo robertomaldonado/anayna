4e53a97d80f6a3ddd0d5968e5aa0f61e6ff3b3d2
Rob <rbt.maldonado@gmail.com>
2020-12-16 23:12:36 -0500

    Added lazos content

--- pages/lazos.vue
+++ pages/lazos.vue
@@ -1,12 +1,69 @@
 <template>
-  <UnderConstruction/>
+  <div>
+    <v-row>
+    <v-col
+      v-for="n in 12" :key="n"
+      class="d-flex child-flex"
+      cols="4" md="3" lg="2" xl="2"
+    >
+      <ItemDisplay 
+        :code="+n"
+        folder="lazos"
+        prefix="LAZ"
+        @click="showImage(`${n}`)"
+      />
+      <v-overlay
+        :z-index="zIndex"
+        :value="overlay"
+      >
+        <v-btn
+          @click="overlay = false"
+        >
+          <v-icon>mdi-close-box</v-icon>
+          Cerrar
+        </v-btn>
+        <v-card>
+          <v-img
+            :src="currentImage"
+            max-height="400"
+            max-width="400"
+            class="grey"
+            @click="overlay = false"
+          >
+          </v-img>
+          <v-card-title class="title text-center-align">
+            <v-spacer/>
+              Code: {{currentCode}}
+            <v-spacer/>
+          </v-card-title>
+        </v-card>
+      </v-overlay>
+    </v-col>
+  </v-row>
+  </div>
 </template>
 
 <script>
-import UnderConstruction from '~/components/catalog/UnderConstruction.vue'
+import ItemDisplay from '~/components/UI/ItemDisplay.vue'
+
 export default {
   components: {
-    UnderConstruction
+    ItemDisplay
+  },
+  data () {
+    return {
+      currentImage: "",
+      currentCode: "",
+      overlay: false,
+      zIndex: 0
+    }
+  },
+  methods: {
+    showImage (item_id) {
+      this.currentImage = `https://storage.googleapis.com/anayna_alpha/lazos/${item_id}.jpg`
+      this.currentCode = `LAZ-${item_id}`
+      this.overlay = !this.overlay
+    }
   }
 }
 </script>
